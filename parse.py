0a826963e6f69505b33c61f6529eb7db0be018f6
Edgar Walden <34025672+tengu53@users.noreply.github.com>
2023-01-16 09:58:25 +0100

    Formátování podle beancountu

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@
 ## Parse vezme XML s výpisem z Monety a udělá z toho CSV soubor(s hodnotami, které využijeme) a také výstup pro Beancount
 
 import xml.etree.ElementTree as ET
+import chime
 ## Načteme XML
 tree = ET.parse('vypis12.xml')
 
@@ -13,11 +14,20 @@ print(type(transactions))
 print(transactions[1].attrib)
 print("---------------------------------------------------------------")
 
-## Vypíšeme datum, amount a zprávu
+## Vypíšeme datum, amount a zprávu a naformátujeme pro beancount, jako výdaj dáme FIXME 
 
 for transaction in transactions:
-    print(transaction.attrib["date-eff"], transaction.attrib["amount"] )
+    print(transaction.attrib["date-eff"], end=" " )
+    print ("*", end=" ")
+    
     texty = transaction.findall('.//trn-message')
     for one in texty:
-        print(one.text)
-    print("-------------------------------") 
+        print('"',one.text,'"')
+    
+    print ("Expenses:FIXME", end="  ")
+
+    print(transaction.attrib["amount"], " CZK")
+    print ("Assets:Moneta")
+    print() 
+
+chime.success()
