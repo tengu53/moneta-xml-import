29034778551c223d5439b37d0eee434f930cc00b
Edgar Walden <34025672+tengu53@users.noreply.github.com>
2024-02-07 10:22:13 +0100

    update

--- pom.py
+++ pom.py
@@ -1,8 +1,1 @@
-# Input number in the format -400,00
-input_number_str = "-400,00"
-
-# Replace comma with dot, remove the leading '-' and convert to float
-output_number = float(input_number_str.replace(',', '').replace('-', ''))
-
-print("Input number:", input_number_str)
-print("Output number:", '{:.2f}'.format(output_number))
+# testovací soubor
