4e3382acf0b807d0f54da69fd8cae3260388ce79
Edgar Walden <34025672+tengu53@users.noreply.github.com>
2024-02-07 08:15:55 +0100

    komentáře

--- parse2.py
+++ parse2.py
@@ -17,19 +17,20 @@ parent_element = root.find('transactions')
 if parent_element is not None:
     # Iterujeme skrz transakce
     for child in parent_element.iterchildren():
-        # Chceme datum
+        
+        # DATUM
         if child.get("date-action") is not "":  # Test, zda není atribut prázdný
             raw_date = child.get('date-action')
         else:
             raw_date = "01.01.1900" # Pokud je atribut prázdný, dáme tam toto datum, podle kterého pak najdeme špatný záznam
-
         # Konvertujeme do ISO formátu
         input_date_obj = datetime.strptime(raw_date, "%d.%m.%Y")
         iso_date = input_date_obj.strftime("%Y-%m-%d")
         
-        # Chceme amount
+        # AMOUNT:
         attribute_value2 = child.get('amount')
-        # Chceme popis transakce - ten je tam blbě, takže musíme takto
+        
+        # POPIS - ten je tam blbě, takže musíme takto
         trn_message_element = child.find('.//trnMessage[@position="2"]')
         if trn_message_element is not None:
             content = trn_message_element.text
@@ -37,7 +38,6 @@ if parent_element is not None:
         # tady to vypíšeme
             # TODO čárku na tečku
             # TODO Naformátovat do Formátu Beancountu        
-        # if attribute_value is not None:
-            print(f"Datum: {iso_date}, Amount: {attribute_value2}, Popis: {content}")
+        print(f"Datum: {iso_date}, Amount: {attribute_value2}, Popis: {content}")
 
 chime.success()
