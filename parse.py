9c4598b0021b42716aa50a6e319386f8fdcab417
Edgar Walden <34025672+tengu53@users.noreply.github.com>
2024-02-09 09:28:25 +0100

    vyčištění, komentáře

--- parse.py
+++ parse.py
@@ -1,33 +1,49 @@
-#! /usr/bin/python3
-## Parse vezme XML s výpisem z Monety a udělá z toho CSV soubor(s hodnotami, které využijeme) a také výstup pro Beancount
-
-import xml.etree.ElementTree as ET
+#! python3
+## Parse vezme XML s výpisem z Monety a udělá z toho co nejvíc srozumitelný výstup pro Beancount
+## Nekategorizuje výdaje - dává tam "FIXME", abych to pak mohl manuálně přiřadit 
+from lxml import etree 
+from datetime import datetime
 import chime
-## Načteme XML
-tree = ET.parse('export.xml')
 
-## Najdeme transakce
-transactions = tree.findall('.//transaction')
+## Načteme XML - ideální postup - v internetovém bankovnictví vyfiltrovat odchozí platby a exportovat do XML
+## Tady zadat XML soubor 
+xml_file = 'export.xml'
 
-## zkouška, co jsme vůbec našli
-print(type(transactions))
-print(transactions[1].attrib)
-print("---------------------------------------------------------------")
+# Tady ho parsujeme
+tree = etree.parse(xml_file)
+root = tree.getroot()
+# Chceme jenom transakce
+parent_element = root.find('transactions')
 
-## Vypíšeme datum, amount a zprávu a naformátujeme pro beancount, jako výdaj dáme FIXME 
-
-for transaction in transactions:
-    datum = transaction.attrib["date-eff"] + " * "
-    print (datum, end="")
-    
-    texty = transaction.findall('.//trn-message')
-    for one in texty:
-        popis='"' + one.text + '"'  
-        print(popis)
-    print ("Expenses:FIXME", end="  ")
-
-    print(transaction.attrib["amount"][1:], " CZK")
-    print ("Assets:Moneta")
-    print() 
+if parent_element != None:
+    # Iterujeme skrz transakce
+    for child in parent_element.iterchildren():
+        
+        # DATUM
+        if child.get("date-action") != "":  # Test, zda není atribut prázdný
+            raw_date = child.get('date-action')
+        else:
+            raw_date = "01.01.1900" # Pokud je atribut prázdný, dáme tam toto datum, podle kterého pak najdeme špatný záznam
+        # Konvertujeme do ISO formátu
+        input_date_obj = datetime.strptime(raw_date, "%d.%m.%Y")
+        iso_date = input_date_obj.strftime("%Y-%m-%d")
+        
+        # AMOUNT:
+        raw_amount = child.get('amount') # Je to string!
+        # Ořízneme "-" pomocí slicingu a vyměníme čárku za tečku   
+        amount = (raw_amount [1:].replace(",","."))
+        # POPIS -v xml je ukrytý do child elementu, takže musíme takto
+        trn_message_element = child.find('.//trnMessage[@position="2"]')
+        if trn_message_element != None:
+            content = trn_message_element.text
+            
+        # Uložíme to do tuple, kdybych s tím chtěl pak ještě nějak pracovat  
+        record = (iso_date, amount, content)
+        
+        # Tady to vypíšeme
+        print(record[0], " *", "\"", record[2],"\"")
+        print ("Expenses:FIXME", record [1], "CZK")
+        print ("Assets:Moneta")
+        print()
 
 chime.success()
--- todo.md
+++ todo.md
@@ -1,8 +1,6 @@
-- [ ] Formátovat výstup
+- [x] Formátovat výstup
 - [ ] připojit výstup do .beancount souboru - na konec
-- [ ] pomocí podmínek co nejvíce polože dobře naformátovat
-- [ ] tam, kde to nepůjde, dát FIXme
 
 ## Časem:
 
-- [ ] Dodat konfigurační soubor - YAML
+- [ ] Doplnit konfigurační soubor - asi YAML - párovat obvyklé platby na základě obsahu content na kategorie výdajů
